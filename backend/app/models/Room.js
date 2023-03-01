dd65095bc44eb7b13c0d46e95e115d6dd80b68ba
vtthinh <vtthinh17st@gmail.com>
2023-03-01 09:02:04 +0700

    Connect DB, create routes, API for Room

---
+++ backend/app/models/Room.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const RoomSchema = new mongoose.Schema(
+    {
+        roomType: {type: Number, required: true,},
+        roomNumber: {type: Number, required: true,},
+        description:{type: [String], required: true},
+        price: {type: Number, required: true,},
+        status: { type: Number, required: true,},
+        dateServe: {type: [Date],},
+        image:{type:[String], required:true},
+    }
+);
+
+export default mongoose.model("Room", RoomSchema);
