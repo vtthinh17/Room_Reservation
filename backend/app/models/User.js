f71f8d11a5fcaafb9d0b87a74742fc74c9ff7f9d
vtthinh <vtthinh17st@gmail.com>
2023-03-03 15:26:42 +0700

    create UserSchema & create API register/login

---
+++ backend/app/models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+    {
+        // require: bat buoc phai co khi dang ky de co tai khoan dang nhap
+        userName: {type: String, required: true,unique: true},
+        passWord: {type: String, required: true},     
+        // optional: khi login co the vao phan profile de them/chinh sua
+        isAdmin: {type: Boolean, default: false},
+        email: {type: String,unique: true},
+        address: {type: String},
+        avatar: {type: String},   
+        phone:{type: String},
+        // chua danh sach cac thong tin phong da dat de confirm/cancel
+        bookingHistory:{type: [String]},
+    },{timestamps:true}
+);
+
+export default mongoose.model("User", UserSchema);
---
+++ backend/app/utils/generateErr.js
@@ -0,0 +1,6 @@
+export const generateError = (status, message) => {
+    const err = new Error();
+    err.status = status;
+    err.message = message;
+    return err;
+  };
